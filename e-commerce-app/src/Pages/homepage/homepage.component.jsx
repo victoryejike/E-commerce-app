0f0a5914d2bbb92e801b5cb5abd2487c75e12a51
Ejike <victoryejike@gmail.com>
2020-12-24 23:46:40 +0100

    broke down the homepage into components and styled them

--- e-commerce-app/src/Pages/homepage/homepage.component.jsx
+++ e-commerce-app/src/Pages/homepage/homepage.component.jsx
@@ -1,41 +1,14 @@
 import React from "react";
+import Directory from "../../Components/directory/directory.component"
 import "./homepage.styles.scss";
 
 const HomePage = () => {
-    <div className="homepage">
-        <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Hats</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">JACKETS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">SNEAKERS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">WOMEN</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">MEN</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
+    return (
+        <div className="homepage">
+            <Directory />
         </div>
-    </div>
+    )
+    
 }
 
 export default HomePage;
