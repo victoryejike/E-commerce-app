340d2d7d4037e8c21cbb95f57c622ab16ca17fd0
Ejike <victoryejike@gmail.com>
2020-12-28 16:01:36 +0100

    implemented sign in with google provider and sign up with email and password

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -5,7 +5,7 @@ import HomePage from './Pages/homepage/homepage.component';
 import ShopPage from "./Pages/shop/shop.component.jsx";
 import Header from "./Components/Header/Header.component";
 import SignInAndSignUpPage from "./Pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor(){
@@ -19,14 +19,28 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user=>{
-      this.setState({ currentUser: user })
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot=>{
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data(),
+            }
+          }, ()=>{
+            console.log(this.state)
+          })
+        })
+      }else{
+        this.setState({ currentUser: userAuth });
+      }
     })
   }
 
   componentWillUnmount(){
-    this.unsubscribefromAuth();
+    this.unsubscribeFromAuth();
   }
 
   render(){
