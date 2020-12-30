937b5419e528de96b8404a21cefdc40f8523e80e
Ejike <victoryejike@gmail.com>
2020-12-30 03:21:55 +0100

    added an action type file to the userReducer and userActions to avoid spelling conflicts

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import HomePage from './Pages/homepage/homepage.component';
 import ShopPage from "./Pages/shop/shop.component.jsx";
 import Header from "./Components/Header/Header.component";
@@ -42,7 +42,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to='/' />) : <SignInAndSignUpPage /> } />
         </Switch>   
       </div>
     );
@@ -50,8 +50,12 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
