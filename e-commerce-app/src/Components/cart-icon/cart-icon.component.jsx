7e32eb5302e9a91ff58109f2816a45312c5d6ec9
Ejike <victoryejike@gmail.com>
2021-01-02 03:13:28 +0100

    created a cartItems array reducer to get the items when the added to cart functionality is clicked on our app

--- e-commerce-app/src/Components/cart-icon/cart-icon.component.jsx
+++ e-commerce-app/src/Components/cart-icon/cart-icon.component.jsx
@@ -6,7 +6,7 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 const CartIcon = ({ toggleCartHidden }) => {
     return (
-        <div className="cart-icon" onClick={toggleCartHidden}>
+        <div className="cart-icon" onClick={()=>toggleCartHidden()}>
             <ShoppingIcon className="shopping-icon" />
             <span className="item-count">0</span>
         </div>
--- e-commerce-app/src/redux/cart/cart.reducer.js
+++ e-commerce-app/src/redux/cart/cart.reducer.js
@@ -2,6 +2,7 @@ import CartActionTypes from './cart.types'
 
 const INITIAL_STATE = {
     hidden: true,
+    cartItems: [],
 }
 
 const cartReducer = (state = INITIAL_STATE, action) =>{
@@ -9,6 +10,11 @@ const cartReducer = (state = INITIAL_STATE, action) =>{
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 hidden: !state.hidden
+            }
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: [...state.cartItems, action.payload]
             }
         default:
             return state;
