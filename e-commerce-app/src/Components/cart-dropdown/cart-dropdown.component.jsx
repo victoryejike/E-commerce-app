9cbeb2a45b65fcdcec1967e6e9f9b9532c3a71a3
Ejike <victoryejike@gmail.com>
2021-01-02 16:27:52 +0100

    repllacing our cartItems in dropdown cart component with memoizied functions from reselect library

--- e-commerce-app/src/Components/cart-dropdown/cart-dropdown.component.jsx
+++ e-commerce-app/src/Components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,7 @@ import "./cart-dropdown.styles.scss";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { connect } from "react-redux";
-import { selectCartItems } from "../../redux/cart/cart.selectors"
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 const CartDropDown = ({ cartItems }) => {
     console.log(cartItems)
